6e28775be8f80c99c175be0e7333d894664ffd47
Ameer Hamza <aha1475@gmail.com>
2022-12-12 17:02:02 +0500

    login page done

--- app/src/main/java/io/notly/android/LoginFragment.kt
+++ app/src/main/java/io/notly/android/LoginFragment.kt
@@ -1,19 +1,29 @@
 package io.notly.android
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import io.notly.android.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.goToSignup.setOnClickListener {
+                findNavController().popBackStack()
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/io/notly/android/RegisterFragment.kt
+++ app/src/main/java/io/notly/android/RegisterFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import io.notly.android.databinding.FragmentRegisterBinding
 
@@ -19,7 +20,9 @@ class RegisterFragment : Fragment() {
     ): View? {
         binding = FragmentRegisterBinding.inflate(inflater, container, false)
 
-
+        binding.goToSignin.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
 
         return binding.root
 
